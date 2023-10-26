a7fbfd1186e409cf17f5bcda98b6686d87f9e81e
Rul Kimi <azrulkimi00@gmail.com>
2023-10-26 15:54:48 +0800

    added menus list

--- src/views/menus/MenusLists.vue
+++ src/views/menus/MenusLists.vue
@@ -7,6 +7,24 @@
       <button>Refresh</button>
       <router-link to="/create">Create New Menu</router-link>
     </div>
-    <ul>List of coaches</ul>
+    <ul v-if="hasMenus">
+      <li v-for="menu in filteredMenus" :key="menu.id">
+        {{ menu.itemName }} - Price: ${{ menu.price }}
+      </li>
+    </ul>
+    <h3 v-else>No menus found</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredMenus() {
+      return this.$store.getters['menus/menus'];
+    },
+    hasMenus() {
+      return this.$store.getters['menus/hasMenus'];
+    }
+  }
+}
+</script>
